a98addf85e61c951d1c0905fe0da924f34b744ab
宋健 <songjian@wikiflyer.cn>
2017-08-13 22:46:59 +0800

    调试

--- lib/aliyun/sms.rb
+++ lib/aliyun/sms.rb
@@ -39,8 +39,7 @@ module Aliyun
           'AccessKeyId' => configuration.access_key_id,
           'Action' => configuration.action,
           'Format' => configuration.format,
-          'ParamString' => message_param,
-          # 'TemplateParam' => message_param,
+          # 'ParamString' => message_param,
           # 'RecNum' => mobile_num,
           'PhoneNumbers' => mobile_num,
           'RegionId' => configuration.region_id,
@@ -50,9 +49,26 @@ module Aliyun
           'SignatureVersion' => configuration.signature_version,
           'TemplateCode' => template_code,
           'Timestamp' => seed_timestamp,
+          'TemplateParam' => message_param,
           'Version' => configuration.sms_version,
         }
       end
+
+      # Signature=zJDF%2BLrzhj%2FThnlvIToysFRq6t4%3D&
+      # AccessKeyId=testId
+      # &Action=SendSms
+      # &Format=XML
+      # &OutId=123
+      # &PhoneNumbers=15300000001
+      # &RegionId=cn-hangzhou
+      # &SignName=%E9%98%BF%E9%87%8C%E4%BA%91%E7%9F%AD%E4%BF%A1%E6%B5%8B%E8%AF%95%E4%B8%93%E7%94%A8
+      # &SignatureMethod=HMAC-SHA1
+      # &SignatureNonce=45e25e9b-0a6f-4070-8c85-2956eda1b466
+      # &SignatureVersion=1.0
+      # &TemplateCode=SMS_71390007
+      # &TemplateParam=%7B%22customer%22%3A%22test%22%7D
+      # &Timestamp=2017-07-12T02%3A42%3A19Z
+      # &Version=2017-05-25
 
       def send(mobile_num, template_code, message_param)
         sms_params = create_params(mobile_num, template_code, message_param)
